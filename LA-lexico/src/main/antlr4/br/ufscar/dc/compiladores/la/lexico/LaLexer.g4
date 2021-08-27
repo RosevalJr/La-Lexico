459195a859d019c9aca2361269abf1734f0939fb
RosevalJr <rdmalajr@gmail.com>
2021-08-27 17:50:27 -0300

    Adicionado reconhecimento de novos tokens e novo error listener.

--- LA-lexico/src/main/antlr4/br/ufscar/dc/compiladores/la/lexico/LaLexer.g4
+++ LA-lexico/src/main/antlr4/br/ufscar/dc/compiladores/la/lexico/LaLexer.g4
@@ -1,12 +1,17 @@
 lexer grammar LaLexer;
 
-PALAVRA_CHAVE: 'algoritmo' | 'declare' | 'literal' | 'inteiro' | 'fim_algoritmo' | 'leia' | 'escreva' | 'real' | 'e' | 'ou' | 'nao' | 'logico' | 'se' | 'senao' | 'fim_se' | 'entao' | 'caso' | 'seja' | '..' | 'fim_caso' | 'para' | 'faca' | 'ate' | 'fim_se' | 'fim_para' | 'enquanto' | 'fim_enquanto';
+PALAVRA_CHAVE: 'algoritmo' | 'declare' | 'literal' | 'inteiro' | 'fim_algoritmo' |
+ 'leia' | 'escreva' | 'real' | 'e' | 'ou' | 'nao' | 'logico' | 'se' | 'senao' |
+ 'fim_se' | 'entao' | 'caso' | 'seja' | '..' | 'fim_caso' | 'para' | 'faca' |
+ 'ate' | 'fim_para' | 'enquanto' | 'fim_enquanto' | 'registro' | 'fim_registro' |
+ 'tipo' | 'procedimento' | 'var' | 'fim_procedimento' | 'retorne' | 'fim_funcao' |
+ 'funcao' | 'constante' |'falso' | 'verdadeiro';
 
-NUM_INT: ('+'|'-')?('0'..'9')+;
+NUM_INT: ('0'..'9')+;
 
-NUM_REAL: ('+'|'-')?('0'..'9')+ '.' ('0'..'9')+;
+NUM_REAL: ('0'..'9')+ '.' ('0'..'9')+;
 
-IDENT: ('a'..'z' | 'A'..'Z')('a'..'z' | 'A'..'Z'|'0'..'9')*;
+IDENT: ('a'..'z' | 'A'..'Z')('a'..'z' | 'A'..'Z'|'0'..'9' | '_')*;
 
 CADEIA: '"' (ESC_SEQ | ~('"' | '\\') )* '"';
 
@@ -17,9 +22,11 @@ COMENTARIO: '{' ~('}')* '}' -> skip;
 
 WS: (' ' | '\t' | '\r' | '\n' ) -> skip;
 
+OP_ARIT: '+' | '-' | '*' | '/' | '%';
+
 OP_REL: '>' | '>=' | '<' | '<=' | '<>' | '=';
 
-OP_ARIT: '+' | '-' | '*' | '/';
+OP_PON: '^' | '&' | '.' | '[' | ']';
 
 DELIM: ':';
 
--- LA-lexico/src/main/java/br/ufscar/dc/compiladores/la/lexico/Principal.java
+++ LA-lexico/src/main/java/br/ufscar/dc/compiladores/la/lexico/Principal.java
@@ -5,30 +5,41 @@
  */
 package br.ufscar.dc.compiladores.la.lexico;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
 
 /**
  *
  * @author moleke
  */
 public class Principal {
-    public static void main(String args[]) throws IOException{
+    public static void main(String args[]) throws IOException, ParseCancellationException {
         CharStream cs = CharStreams.fromFileName(args[0]);
         LaLexer lex = new LaLexer(cs);
-        FileWriter myWriter = new FileWriter(args[1]);
         
-        Token t = null;
+        FileWriter myWriter = new FileWriter(args[1]);
+        try{
+            lex.removeErrorListeners();
+            lex.addErrorListener(ErrorListenerLA.INSTANCE);
+        
+            Token t = null;
 
-        while(Token.EOF != (t = lex.nextToken()).getType()){
+            while(Token.EOF != (t = lex.nextToken()).getType()){
 
-            if(t.getType() == 1 | (t.getType() >= 8 && t.getType() <= 14))
-                myWriter.write("<'"+t.getText()+"','"+t.getText()+"'>\n");
-            else
-                myWriter.write("<'"+t.getText()+"',"+LaLexer.VOCABULARY.getDisplayName(t.getType())+">\n");
+                if(t.getType() == 1 | (t.getType() >= 8 && t.getType() <= 15))
+                    myWriter.write("<'"+t.getText()+"','"+t.getText()+"'>\n");
+                else
+                    myWriter.write("<'"+t.getText()+"',"+LaLexer.VOCABULARY.getDisplayName(t.getType())+">\n");
+            }
+        } catch(ParseCancellationException e){
+            myWriter.write(e.getMessage());
         }
         myWriter.close();
     }
